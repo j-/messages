e32af065bb6ea6c6103fab46f5c9fe3e5bd934ff
j- <j@skeoh.com>
2017-04-23 18:24:33 +1000

    Add "create message" action

--- src/actions.ts
+++ src/actions.ts
@@ -2,10 +2,20 @@ import {
 	INode,
 } from './node';
 
+import {
+	IMessage,
+} from './message';
+
 export interface IAction {}
 
 export class ActionCreateNode implements IAction {
 	constructor (
 		public readonly node: Partial<INode>,
+	) {}
+}
+
+export class ActionCreateMessage implements IAction {
+	constructor (
+		public readonly message: Partial<IMessage>,
 	) {}
 }
--- src/engine.ts
+++ src/engine.ts
@@ -3,13 +3,19 @@ import { v4 as uuid } from 'uuid';
 import {
 	IAction,
 	ActionCreateNode,
+	ActionCreateMessage,
 } from './actions';
 
 import {
 	INode,
 } from './node';
 
+import {
+	IMessage,
+} from './message';
+
 export async function execute (action: ActionCreateNode): Promise<INode>;
+export async function execute (action: ActionCreateMessage): Promise<IMessage>;
 export async function execute (action: IAction): Promise<any> {
 	if (action instanceof ActionCreateNode) {
 		const node: INode = {
@@ -18,6 +24,13 @@ export async function execute (action: IAction): Promise<any> {
 			dateCreated: Date.now(),
 		};
 		return node;
+	} else if (action instanceof ActionCreateMessage) {
+		const message: IMessage = {
+			...action.message,
+			id: uuid(),
+			dateCreated: Date.now(),
+		};
+		return message;
 	} else {
 		throw new Error('Unrecognised action');
 	}
