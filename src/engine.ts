23b321b98b4c3d69fab6ae781004520d9394f542
j- <j@skeoh.com>
2017-04-23 17:42:30 +1000

    Declare `execute` return type explicitly

--- src/engine.ts
+++ src/engine.ts
@@ -3,6 +3,7 @@ import {
 	ActionHelloWorld,
 } from './actions';
 
+export async function execute (action: ActionHelloWorld): Promise<string>;
 export async function execute (action: IAction) {
 	if (action instanceof ActionHelloWorld) {
 		return 'Hello world';
