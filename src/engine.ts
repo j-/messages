229a42fc6a593c7e47911c7743ce1b80a652e3d1
j- <j@skeoh.com>
2017-04-26 19:37:40 +1000

    Abstract action execution logic

--- src/engine.ts
+++ src/engine.ts
@@ -20,22 +20,30 @@ export async function execute (action: IActionCreateNode): Promise<INode>;
 export async function execute (action: IActionCreateMessage): Promise<IMessage>;
 export async function execute (action: IAction): Promise<any> {
 	if (isCreateNodeAction(action)) {
-		const partialNode = action.node;
-		const node: INode = {
-			type: partialNode.type,
-			id: uuid(),
-			dateCreated: Date.now(),
-			...partialNode,
-		};
-		return node;
+		return executeCreateNode(action);
 	} else if (isCreateMessageAction(action)) {
-		const message: IMessage = {
-			...action.message,
-			id: uuid(),
-			dateCreated: Date.now(),
-		};
-		return message;
+		return executeCreateMessage(action);
 	} else {
 		throw new Error('Unrecognised action');
 	}
+}
+
+export async function executeCreateNode (action: IActionCreateNode): Promise<INode> {
+	const partialNode = action.node;
+	const node: INode = {
+		type: partialNode.type,
+		id: uuid(),
+		dateCreated: Date.now(),
+		...partialNode,
+	};
+	return node;
+}
+
+export async function executeCreateMessage (action: IActionCreateMessage): Promise<IMessage> {
+	const message: IMessage = {
+		...action.message,
+		id: uuid(),
+		dateCreated: Date.now(),
+	};
+	return message;
 }
