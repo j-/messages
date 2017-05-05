2938dabb2de2c1901ba5052a4a84f9035b786900
j- <j@skeoh.com>
2017-05-05 19:40:39 +1000

    Add a "create node" API endpoint

--- src/api/nodes.ts
+++ src/api/nodes.ts
@@ -1,6 +1,10 @@
 import * as express from 'express';
 import { execute } from '../engine';
-import { IActionCatMessages } from '../actions';
+
+import {
+	IActionCatMessages,
+	IActionCreateNode,
+} from '../actions';
 
 const app: express.Express = express();
 
@@ -27,6 +31,61 @@ app.get('/:ids/messages', async (req, res) => {
 		}
 	} catch (err) {
 		if (err.code === 'SQLITE_ERROR') {
+			const status = 500;
+			const response = {
+				error: true,
+				message: 'Error communicating with database',
+			};
+			res.status(status).send(response);
+		} else {
+			const status = 500;
+			const response = {
+				error: true,
+				message: 'An unknown error occurred',
+			};
+			res.status(status).send(response);
+			console.error(err);
+			process.exit(1);
+		}
+	}
+});
+
+app.post('/', async (req, res) => {
+	const action: IActionCreateNode = {
+		type: 'CreateNode',
+		node: req.body,
+	};
+	try {
+		const node = await execute(action);
+		if (node) {
+			const status = 201;
+			const response = {
+				result: node,
+			};
+			res.status(status).send(response);
+		} else {
+			const status = 204;
+			const response = {
+				result: <any>null,
+			};
+			res.status(status).send(response);
+		}
+	} catch (err) {
+		if (err.name === 'UnrecognizedNodeTypeError') {
+			const status = 400;
+			const response = {
+				error: true,
+				message: err.message,
+			};
+			res.status(status).send(response);
+		} else if (err.name === 'InvalidPropertyError') {
+			const status = 400;
+			const response = {
+				error: true,
+				message: err.message,
+			};
+			res.status(status).send(response);
+		} else if (err.code === 'SQLITE_ERROR') {
 			const status = 500;
 			const response = {
 				error: true,
--- src/engine/create-node.ts
+++ src/engine/create-node.ts
@@ -1,4 +1,6 @@
 import { v4 as uuid } from 'uuid';
+import { Transaction } from 'knex';
+import { db } from '../connection';
 
 import {
 	IActionCreateNode,
@@ -9,15 +11,55 @@ import {
 	isNodeTypeValid,
 } from '../node';
 
+export class InvalidPropertyError extends Error {
+	constructor (message: string) {
+		super(message);
+		this.name = 'InvalidPropertyError';
+	}
+}
+
+export class UnrecognizedNodeTypeError extends Error {
+	constructor (message: string) {
+		super(message);
+		this.name = 'UnrecognizedNodeTypeError';
+	}
+}
+
 export async function executeCreateNode (action: IActionCreateNode): Promise<INode> {
 	const partialNode = action.node;
-	if (partialNode.id) {
-		throw new Error('Cannot create a node that already has an ID');
-	} else if (partialNode.dateCreated) {
-		throw new Error('Cannot create a node that already has a created date');
-	} else if (!isNodeTypeValid(partialNode.type)) {
-		throw new TypeError(`Did not recognize node type "${partialNode.type}"`);
+	validatePartialNode(partialNode);
+	const node = createNodeFromPartial(partialNode);
+	await db.transaction((tx) => Promise.all([
+		// Create node record
+		insertNode(tx, node),
+	]));
+	return node;
+}
+
+export function validatePartialNode (partialNode: Partial<INode>) {
+	if (!partialNode.type) {
+		throw new InvalidPropertyError('Node must be created with a type');
 	}
+	const keys = Object.keys(partialNode);
+	for (const key of keys) {
+		switch (key) {
+			case 'id':
+				throw new InvalidPropertyError('Cannot create a node that already has an ID');
+			case 'dateCreated':
+				throw new InvalidPropertyError('Cannot create a node that already has a created date');
+			case 'type':
+				if (!isNodeTypeValid(partialNode.type)) {
+					throw new UnrecognizedNodeTypeError(`Did not recognize node type "${partialNode.type}"`);
+				} else {
+					break;
+				}
+			default:
+				throw new InvalidPropertyError(`Did not recognize property with name "${key}"`);
+		}
+	}
+}
+
+export function createNodeFromPartial (partialNode: Partial<INode>) {
 	const node: INode = {
 		...partialNode,
 		type: partialNode.type,
@@ -25,4 +67,20 @@ export async function executeCreateNode (action: IActionCreateNode): Promise<INo
 		dateCreated: Date.now(),
 	};
 	return node;
+}
+
+export async function insertNode (tx: Transaction, node: INode): Promise<void> {
+	return tx.table('node').insert({
+		id: node.id,
+		node_type: await getNodeTypeId(tx, node.type),
+	});
+}
+
+export async function getNodeTypeId (tx: Transaction, nodeType: string): Promise<number> {
+	const rows = await tx.table('node_types').select('id').where('node_type', '=', nodeType);
+	if (rows.length === 0) {
+		const err = new UnrecognizedNodeTypeError(`Did not recognize node type "${nodeType}"`);
+	} else {
+		return Number(rows[0].id);
+	}
 }
