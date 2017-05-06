dc23309945a476ffe9e1b4cc79f9951c359e6d2a
j- <j@skeoh.com>
2017-05-06 12:39:46 +1000

    Test fetching messages from other nodes

--- test/api/nodes.test.js
+++ test/api/nodes.test.js
@@ -56,6 +56,137 @@ test('Can get messages from CatNode', async (t) => {
 	});
 });
 
+test('Can get messages from multiple CatNodes', async (t) => {
+	t.plan(2);
+	const res = await request(app).get('/99e0c2cb-0d46-49aa-afe2-898f0f5af337+afa0319a-9391-4c27-a7fe-e50f226ce735/messages');
+	t.is(res.status, 200);
+	t.deepEqual(res.body, {
+		"result": [
+			{
+				"id": "965d981b-58a8-4921-9783-ac52dba84012",
+				"title": "Get on down to Real Fake Doors",
+				"body": "Fill a whole room up with them",
+				"tag": null,
+				"icon": null,
+				"data": null,
+				"url": null,
+				"timestamp": null,
+				"dateCreated": 1493213001576,
+				"dateModified": null
+			},
+			{
+				"id": "c142fc66-00ea-4675-be4a-268450b270ce",
+				"title": "Hey are you tired of real doors cluttering up your house",
+				"body": "Where you open them and you actually go somewhere?",
+				"tag": null,
+				"icon": null,
+				"data": null,
+				"url": null,
+				"timestamp": null,
+				"dateCreated": 1493212993511,
+				"dateModified": null
+			},
+			{
+				"id": "b23cb669-5e8f-402a-b9d8-df1a0138ef5b",
+				"title": "I'm Ants in my Eyes Johnson",
+				"body": "Here at Ants in my Eyes Jonson's Electronics",
+				"tag": null,
+				"icon": null,
+				"data": null,
+				"url": null,
+				"timestamp": null,
+				"dateCreated": 1493212943841,
+				"dateModified": null
+			},
+			{
+				"id": "cbe91a28-70a8-4878-8449-daaef3141d3e",
+				"title": "Hello world",
+				"body": "This is the first message",
+				"tag": null,
+				"icon": null,
+				"data": {
+					"foo": "bar",
+					"hello": "world"
+				},
+				"url": null,
+				"timestamp": null,
+				"dateCreated": 1493212879111,
+				"dateModified": null
+			}
+		]
+	});
+});
+
+test('Can get messages from nodes in any order', async (t) => {
+	t.plan(2);
+	const res = await request(app).get('/afa0319a-9391-4c27-a7fe-e50f226ce735+99e0c2cb-0d46-49aa-afe2-898f0f5af337/messages');
+	t.is(res.status, 200);
+	t.deepEqual(res.body, {
+		"result": [
+			{
+				"id": "965d981b-58a8-4921-9783-ac52dba84012",
+				"title": "Get on down to Real Fake Doors",
+				"body": "Fill a whole room up with them",
+				"tag": null,
+				"icon": null,
+				"data": null,
+				"url": null,
+				"timestamp": null,
+				"dateCreated": 1493213001576,
+				"dateModified": null
+			},
+			{
+				"id": "c142fc66-00ea-4675-be4a-268450b270ce",
+				"title": "Hey are you tired of real doors cluttering up your house",
+				"body": "Where you open them and you actually go somewhere?",
+				"tag": null,
+				"icon": null,
+				"data": null,
+				"url": null,
+				"timestamp": null,
+				"dateCreated": 1493212993511,
+				"dateModified": null
+			},
+			{
+				"id": "b23cb669-5e8f-402a-b9d8-df1a0138ef5b",
+				"title": "I'm Ants in my Eyes Johnson",
+				"body": "Here at Ants in my Eyes Jonson's Electronics",
+				"tag": null,
+				"icon": null,
+				"data": null,
+				"url": null,
+				"timestamp": null,
+				"dateCreated": 1493212943841,
+				"dateModified": null
+			},
+			{
+				"id": "cbe91a28-70a8-4878-8449-daaef3141d3e",
+				"title": "Hello world",
+				"body": "This is the first message",
+				"tag": null,
+				"icon": null,
+				"data": {
+					"foo": "bar",
+					"hello": "world"
+				},
+				"url": null,
+				"timestamp": null,
+				"dateCreated": 1493212879111,
+				"dateModified": null
+			}
+		]
+	});
+});
+
+test('Cannot get messages from ReadNode', async (t) => {
+	t.plan(2);
+	const res = await request(app).get('/8c5c1e89-b523-4e2d-93c4-274f1c4baa6f/messages');
+	t.is(res.status, 200);
+	t.deepEqual(res.body, {
+		"result": []
+	});
+});
+
 test('Can create CatNode', async (t) => {
 	t.plan(5);
 	const res = await request(app).post('/').send({ type: 'CatNode' });
