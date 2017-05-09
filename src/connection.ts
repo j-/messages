a35cb711346d2a5237c04e3121864d1d46e3f888
j- <j@skeoh.com>
2017-05-09 20:59:02 +1000

    Document the database connection members

--- src/connection.ts
+++ src/connection.ts
@@ -5,8 +5,15 @@ const knexfile = require('../db/knexfile');
 const env = process.env.NODE_ENV || 'development';
 const config = knexfile[env];
 
+/**
+ * The main database connection object. Connects automatically.
+ */
 export const db = knex(config);
 
+/**
+ * Disconnect from the active database. Should only be called when shutting
+ * down the server.
+ */
 export function destroy () {
 	return Promise.resolve<void>(db.destroy());
 }
