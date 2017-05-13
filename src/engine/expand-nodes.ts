4e0bd94c879129cff1a25604c1aef08237fff02d
j- <j@skeoh.com>
2017-05-13 16:23:10 +1000

    Implement "get child node IDs" logic

--- src/engine/expand-nodes.ts
+++ src/engine/expand-nodes.ts
@@ -9,5 +9,24 @@ import {
 } from '../types';
 
 export async function executeExpandNodes (action: IActionExpandNodes): Promise<UUID[]> {
-	throw new Error('Not yet implemented');
+	const result: UUID[] = [];
+	let parentNodeIds: UUID[] = action.nodeIds;
+	while (true) {
+		const ids = await getChildNodeIds(parentNodeIds, result);
+		if (ids.length > 0) {
+			result.push(...ids);
+			parentNodeIds = ids;
+		} else {
+			break;
+		}
+	}
+	return result;
+}
+
+export async function getChildNodeIds (parentIds: UUID[], blackListIds: UUID[] = []): Promise<UUID[]> {
+	const result: UUID[] = await db('node_cat')
+		.column('input_node_id')
+		.where('output_node_id', 'in', parentIds)
+		.andWhereNot('output_node_id', 'in', blackListIds);
+	return result;
 }
