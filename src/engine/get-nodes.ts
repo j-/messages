262515afaba08ff80fc26edf4915ab4cc61f7c08
j- <j@skeoh.com>
2017-05-13 21:03:02 +1000

    Return list of concatenated nodes when querying CatNodes
    Closes #10

--- src/engine/get-nodes.ts
+++ src/engine/get-nodes.ts
@@ -1,4 +1,5 @@
 import { db } from '../connection';
+import { QueryBuilder } from 'knex';
 
 import {
 	IActionGetNodes,
@@ -6,15 +7,57 @@ import {
 
 import {
 	INode,
+	ICatNode,
+	isCatNode,
 } from '../node';
 
+import {
+	UUID,
+} from '../types';
+
 export async function executeGetNodes (action: IActionGetNodes): Promise<INode[]> {
-	const rows: INode[] = await db('node')
-		.join('node_types', 'node_types.id', '=', 'node.node_type')
-		.where('node.id', 'in', action.nodeIds)
+	const { nodeIds } = action;
+	const result = await db.transaction((tx) => Promise.all([
+		getNodeData(tx, action.nodeIds),
+		getNodeConcatMap(tx, action.nodeIds),
+	]));
+	const nodes: INode[] = result[0];
+	const concatMap: NodeConcatMap = result[1];
+	nodes.filter(isCatNode).forEach((node: ICatNode) => {
+		node.concatenates = concatMap[node.id];
+	});
+	return nodes;
+}
+
+export async function getNodeData (db: QueryBuilder, nodeIds: UUID[]): Promise<INode[]> {
+	const rows: INode[] = await db.table('node')
+		.join('node_types', 'node_types.id', 'node.node_type')
+		.whereIn('node.id', nodeIds)
 		.select([
 			'node.id',
 			'node_types.type_name as type',
 		]);
 	return rows;
+}
+
+export interface NodeConcatMap {
+	[outputId: string]: string[];
+}
+
+export async function getNodeConcatMap (db: QueryBuilder, nodeIds: UUID[]): Promise<NodeConcatMap> {
+	const rows = await db.table('node_cat')
+		.whereIn('node_cat.output_node_id', nodeIds)
+		.select([
+			'node_cat.output_node_id as outputId',
+			'node_cat.input_node_id as inputId',
+		]);
+	// Map of inputs (as an array) that correspond to an output (index)
+	const map: NodeConcatMap = {};
+	for (const row of rows) {
+		const { inputId, outputId } = row;
+		const concatenates: UUID[] = map[outputId] || [];
+		concatenates.push(inputId);
+		map[outputId] = concatenates;
+	}
+	return map;
 }
