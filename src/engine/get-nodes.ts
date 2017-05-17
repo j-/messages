c6e4ec3a9527fbabcfb144a76d9f727bfc409a43
j- <j@skeoh.com>
2017-05-17 21:23:24 +1000

    Add docs to new "get nodes" module

--- src/engine/get-nodes.ts
+++ src/engine/get-nodes.ts
@@ -15,6 +15,9 @@ import {
 	UUID,
 } from '../types';
 
+/**
+ * Get details about a given set of nodes.
+ */
 export async function executeGetNodes (action: IActionGetNodes): Promise<INode[]> {
 	const { nodeIds } = action;
 	const result = await db.transaction((tx) => Promise.all([
@@ -29,6 +32,9 @@ export async function executeGetNodes (action: IActionGetNodes): Promise<INode[]
 	return nodes;
 }
 
+/**
+ * Get information that is common to all nodes -- ID, type etc.
+ */
 export async function getNodeData (db: QueryBuilder, nodeIds: UUID[]): Promise<INode[]> {
 	const rows: INode[] = await db.table('node')
 		.join('node_types', 'node_types.id', 'node.node_type')
@@ -40,10 +46,33 @@ export async function getNodeData (db: QueryBuilder, nodeIds: UUID[]): Promise<I
 	return rows;
 }
 
+/**
+ * Maps input node IDs against a single output node ID.
+ */
 export interface NodeConcatMap {
+	/**
+	 * Key is an output node ID. Value is an array of input node IDs.
+	 *
+	 * The node with the ID in the key concatenates all the nodes with the IDs
+	 * in the value.
+	 *
+	 * @example
+	 *
+	 *     {
+	 *       "99e0c2cb-0d46-49aa-afe2-898f0f5af337": [
+	 *         "ccf90446-3267-4c59-9472-0f2043f3501c",
+	 *         "10eb2385-c8e5-4bea-be09-dc26cae237af",
+	 *         "e92d26e5-d10a-41c7-9439-dbc7d01161e4"
+	 *     }
+	 */
 	[outputId: string]: string[];
 }
 
+/**
+ * Returns a map of output node IDs (as keys) and the input node IDs they
+ * concatenate (as an array). Used to quickly look up all the nodes that a
+ * particular CatNode concatenates.
+ */
 export async function getNodeConcatMap (db: QueryBuilder, nodeIds: UUID[]): Promise<NodeConcatMap> {
 	const rows = await db.table('node_cat')
 		.whereIn('node_cat.output_node_id', nodeIds)
